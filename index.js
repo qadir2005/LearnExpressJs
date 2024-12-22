37f92ee3d032c8c20f90cfdd895933fe1b8c8540
Abdul Qadir <153851145+qadir2005@users.noreply.github.com>
2024-12-22 14:38:02 +0500

    some changing

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/:id', (req, res) => {
+  res.send(req.params.id)
+  console.log(req.params);
+  
+})
+app.get("/html", (req,res)=>{
+    res.sendFile()
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
