c030dd21cf8ff3f501397b19b90676d617b7e080
Abdul Qadir <153851145+qadir2005@users.noreply.github.com>
2024-12-25 13:54:37 +0500

    some changing

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/:id', (req, res) => {
+app.get('/', (req, res) => {
   res.send(req.params.id)
   console.log(req.params);
   
 })
-app.get("/html", (req,res)=>{
-    res.sendFile()
-})
+// app.get("/html", (req,res)=>{
+//     res.sendFile()
+// })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
